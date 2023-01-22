62582745f2dbbf9e1dff4e1cd6820afed739d5e4
Shahbaztaj <123319969+Shahbaztaj@users.noreply.github.com>
2023-01-22 23:11:55 +0530

    Added-print

---
+++ 01-Functions/Print.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import static java.lang.Math.ceil;
+
+public class Print {
+
+    //Make a function here and call it in main()
+    public static void fun() {
+        // write code here
+		System.out.println("I am another function");
+    }
+    public static void main(String[] args) {
+        fun();
+    }
+}
